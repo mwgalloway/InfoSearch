d5f4f73ada33029cd3dfc4151c1c7a7954f4833c
Matthew Galloway <email": null>
2016-11-03 21:14:47 -0500

    Wrote scrape_all method
    This method will iterate through our list of links and scrape links
    into an array of new links. When it completes the list, it is wiped
    and the new links are written to the file.

--- crawler/crawler.rb
+++ crawler/crawler.rb
@@ -2,8 +2,21 @@ class Crawler
   attr_accessor :links_to_scrape, :link_bucket, :current_url
   def initialize
     @links_to_scrape = []
-    File.foreach("links") {|link| self.links_to_scrape << link}
+    File.foreach("links") {|link| self.links_to_scrape << link.chomp}
     @link_bucket = []
+    self.scrape_all
+  end
+
+  def scrape_all
+    links_to_scrape[0..-2].each do |link|
+      self.current_url = link
+      p self.current_url
+      self.scrape
+    end
+    self.link_bucket.uniq!
+    File.open("links", "w+") do |f|
+      self.link_bucket.each {|link| f.puts(link)}
+    end
   end
 
   def scrape
@@ -16,18 +29,21 @@ class Crawler
     !url.match(/^https?:\/\//i)
   end
 
-  def concat_relative(parent, relative)
-    if relative[0] == "/"
-      parent + relative[1..-1]
+  def concat_relative(parent, link)
+    return link unless url_relative?(link)
+    if link[0] == "/"
+      parent + link[1..-1]
     else
-      parent + relative
+      parent + link
     end
   end
 
   def parse_links(noko_doc)
     parent_directory = self.current_url[0..self.current_url.rindex("/")]
-    links = noko_doc.css('a').map { |link| link ["href"] }
-    links.select { |link| url_relative?(link) }.each{ |link| link = concat_relative(parent_directory, link) }
+    links = noko_doc.css('a').map{ |link| link ["href"] }
+    p links
+    links.map!{ |link| concat_relative(parent_directory, link) }
+    self.link_bucket += links
   end
 
 end
