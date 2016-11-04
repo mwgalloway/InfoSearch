f111ff8e6eeec167455e990d8b3510c90eef8bd7
Kyle Dorsey & Matthew Galloway <pair+kylepdorsey.mwgalloway@devbootcamp.com>
2016-11-04 18:38:37 -0500

    Print results

--- app/controllers/pages.rb
+++ app/controllers/pages.rb
@@ -4,7 +4,15 @@ get '/pages' do
   erb :'index'
 end
 
-post '/pages' do
-
-  @results = Word.find_by(text: params[:search_input])
+get '/pages/search' do
+  result = Word.find_by(text: params[:search_input])
+  if result
+    @results = result.metrics.sort {|a,b| b.ranking <=> a.ranking}
+  else
+    @results = ["We could not find anything that met your search terms"]
+  end
+  erb :'index'
 end
+
+# word1 = Word.find_by(text: "Schiller")
+# word1.metrics.sort {|a,b| b.ranking <=> a.ranking}
