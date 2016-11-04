04b1c6f28ea2af22cb37608828f8f0cdb6375699
Matthew Galloway <email": null>
2016-11-03 20:43:43 -0500

    Write concat_relative method
    Wrote a method to contatanate the parent url along with a matching
    relative path. This method will ensure that any leading slashes on
    the relative path are removed.

--- crawler/crawler.rb
+++ crawler/crawler.rb
@@ -1,10 +1,9 @@
 class Crawler
-  attr_accessor :links_to_scrape, :link_bucket
+  attr_accessor :links_to_scrape, :link_bucket, :current_url
   def initialize
     @links_to_scrape = []
     File.foreach("links") {|link| self.links_to_scrape << link}
     @link_bucket = []
-    @current_url = ''
   end
 
   def scrape
@@ -17,10 +16,18 @@ class Crawler
     !url.match(/^https?:\/\//i)
   end
 
+  def concat_relative(parent, relative)
+    if relative[0] == "/"
+      parent + relative[1..-1]
+    else
+      parent + relative
+    end
+  end
+
   def parse_links(noko_doc)
     parent_directory = self.current_url[0..self.current_url.rindex("/")]
     links = noko_doc.css('a').map { |link| link ["href"] }
-    links.select { |link| url_relative?(link) }.each {|link| link = parent_directory + link }
+    links.select { |link| url_relative?(link) }.each{ |link| link = concat_relative(parent_directory, link) }
   end
 
 end
