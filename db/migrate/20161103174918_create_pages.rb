74f9dc375a7f7ac16f5c049d8a34fc3b53141026
Matthew Galloway <email": null>
2016-11-04 18:36:32 -0500

    Write add_to_index Page class method
    This method will take a hash containing a url and a nokogiri document
    and instatiate a new page along with any words and metrics needed.
    It will return true if the url is new, and false if a record already
    exists.

--- db/migrate/20161103174918_create_pages.rb
+++ db/migrate/20161103174918_create_pages.rb
@@ -2,7 +2,7 @@ class CreatePages < ActiveRecord::Migration
   def change
     create_table :pages do |t|
       t.string :url, null: false, unique: true
-      t.string :title, null: false
+      t.string :title
 
       t.timestamps(null: false)
     end
