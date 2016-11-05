8f89776735a96a2f830541cb2c5c22a8c8701e7d
John Moore <johnmoore314@gmail.com>
2016-11-05 18:58:13 -0500

    Add functionality to multi-word search, needs refinement from edge case of potentially having multi words that are not part of the database

--- app/controllers/pages.rb
+++ app/controllers/pages.rb
@@ -5,12 +5,41 @@ get '/pages' do
 end
 
 get '/pages/search' do
-  result = Word.find_by(text: params[:search_input])
-  if result
-    @results = result.metrics.sort {|a,b| b.ranking <=> a.ranking}
-  else
-    @results = ["We could not find anything that met your search terms"]
+  query_words = []
+  inputs = params[:search_input].split(" ")
+  inputs.each do |input|
+    found_word = Word.find_by(text: input)
+    if found_word
+      query_words << found_word
+    end
   end
+  p ' ====================== '
+  p query_words
+  p ' ====================== '
+  if query_words != []
+    final_results = []
+
+    query_words.each do |word|
+      query_results = word.metrics
+
+      query_results.each do |result|
+        found_object = final_results.select { |object| object[:page] == result.page }
+
+        if found_object != []
+          final_results.delete_if { |object| object[:page] == result.page }
+          found_object[0][:ranking] += result.ranking
+          final_results << found_object[0]
+        else
+          final_results << { object: result, page: result.page, ranking: result.ranking }
+        end
+      end
+      
+      @results = final_results.sort { |a,b| b[:ranking] <=> a[:ranking] }
+    end
+  else
+    @error = ["We could not find anything that met your search terms"]
+  end
+
   erb :'index'
 end
 
