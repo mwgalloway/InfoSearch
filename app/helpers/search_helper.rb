8c999dc03ae7431ba9da34667e1749be14aa0f88
John Michael Moore <johnmoore314@gmail.com>
2016-11-06 14:19:17 -0600

    Refactor search helper for better naming convention on finding pages for results with a separate method that will combine page rankings

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -18,27 +18,31 @@ module SearchHelper
       finalized_results = []
       query_words_array.each do |query_word|
         query_results = query_word.metrics
-        populate_results_in_hash(query_results, finalized_results)
+        find_page_objects_and_rankings(query_results, finalized_results)
       end
       finalized_results
     end
   end
 
-  def populate_results_in_hash(query_results, finalized_results)
+  def find_page_objects_and_rankings(query_results, finalized_results)
     query_results.each do |result|
       found_object = finalized_results.select { |object| object[:page] == result.page }
 
       if found_object != []
-        finalized_results.delete_if { |object| object[:page] == result.page }
-
-        found_object[0][:ranking] += result.ranking
-
-        finalized_results << found_object[0]
+        combine_page_rankings(found_object, finalized_results, result)
       else
         finalized_results << { object: result, page: result.page, ranking: result.ranking }
       end
     end
     finalized_results
+  end
+
+  def combine_page_rankings(found_object, finalized_results, result)
+    finalized_results.delete_if { |object| object[:page] == result.page }
+
+    found_object[0][:ranking] += result.ranking
+
+    finalized_results << found_object[0]
   end
 end
 
