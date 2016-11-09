319d950e52cffd5f0c026057816d6b352b1f2acb
David Rodgers & Kyle Dorsey <pair+dtrodger.kylepdorsey@devbootcamp.com>
2016-11-09 16:03:26 -0600

    Remove AR files, from db and some from environment

--- app/controllers/pages.rb
+++ app/controllers/pages.rb
@@ -5,16 +5,16 @@ get '/pages' do
 end
 
 get '/pages/search/:page' do
+  @benchmark = Benchmark.realtime{
   @current_page = params[:page].to_i
   skip = @current_page * 10
-  @benchmark = Benchmark.realtime{
   @results = Page.search(params[:search_input], skip)
-  }
   @results_count = @results.count
   if @results_count == 0
     @results = []
     @error = ["We could not find anything that met your search terms"]
   end
 
+  }
   erb :'index'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -66,4 +66,8 @@ Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
-require APP_ROOT.join('config', 'database')
+Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
+  filename = File.basename(model_file).gsub('.rb', '')
+  autoload ActiveSupport::Inflector.camelize(filename), model_file
+end
+# require APP_ROOT.join('config', 'database')
